527bf0afe3ae548d43ce06637dd4a60c68f459ad
Nick Day <thenickday@icloud.com>
2019-12-05 00:13:16 -0500

    updated/changed main App file

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
 import React, { Component } from 'react';
-
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from 'slugify';
-
-import './App.css';
-
-// This object will allow us to
-// easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-});
+import "./App.css";
+import Feature from "./components/feature/Feature";
+import FeatureOption from "./components/featureOption/FeatureOption";
+import SummaryOption from "./components/summaryOption/SummaryOption";
 
 class App extends Component {
-  state = {
-    selected: {
-      Processor: {
-        name: '17th Generation Intel Core HB (7 Core with donut spare)',
-        cost: 700
-      },
-      'Operating System': {
-        name: 'Ubuntu Linux 16.04',
-        cost: 200
-      },
-      'Video Card': {
-        name: 'Toyota Corolla 1.5v',
-        cost: 1150.98
-      },
-      Display: {
-        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-        cost: 1500
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: {
+        Processor: {
+          name: "17th Generation Intel Core HB (7 Core with donut spare)",
+          cost: 700
+        },
+        "Operating System": {
+          name: "Ubuntu Linux 16.04",
+          cost: 200
+        },
+        "Video Card": {
+          name: "Toyota Corolla 1.5v",
+          cost: 1150.98
+        },
+        Display: {
+          name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+          cost: 1500
+        }
       }
-    }
-  };
+    };
+  }
 
   updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
@@ -44,74 +38,55 @@ class App extends Component {
   };
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
-
-    const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.state.selected[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
+    const summary = Object.keys(this.state.selected).map(key => (
+      <SummaryOption option={this.state.selected[key]} specType={key} />
+    ));
 
     const total = Object.keys(this.state.selected).reduce(
       (acc, curr) => acc + this.state.selected[curr].cost,
       0
     );
 
+    const features = Object.keys(this.props.features).map(key => {
+      const options = this.props.features[key].map((item, index) => {
+        return (
+          <FeatureOption
+            featureKey={key}
+            id={key + "_" + index}
+            item={item}
+            index={index}
+            key={key + "_" + index}
+            optionName={this.state.selected[key].name}
+            updateFeature={this.updateFeature}
+          />
+        );
+      });
+
+      return <Feature options={options} featureKey={key} key={key} />;
+    });
+
     return (
       <div className="App">
         <header>
-          <h1>ELF Computing | Laptops</h1>
+          <h1>ELF Computing</h1>
+          <h3>Laptops</h3>
+          <h5>Customize your laptop</h5>
         </header>
         <main>
-          <form className="main__form">
-            <h2>Customize your laptop</h2>
+          <section className="main__form">
+            <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
             {features}
-          </form>
+          </section>
           <section className="main__summary">
-            <h2>Your cart</h2>
+            <h3>NEW GREENLEAF 2018</h3>
             {summary}
             <div className="summary__total">
-              <div className="summary__total__label">Total</div>
+              <div className="summary__total__label">Your Price: </div>
               <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
+                {new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD"
+                }).format(total)}
               </div>
             </div>
           </section>
